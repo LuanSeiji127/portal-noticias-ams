1ace162dda15b0a127f1801ec7f93c54d4693a83
luan-seiji <luanseiji127@gmail.com>
2026-08-24 13:31:33 -0300

    QULAQUER VCOISA

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Portal de notícias carregado com sucesso!");
+
+let nomevisitante = prompt("Qual é o seu nome?");
+alert("Olá," + nomevisitante + "! Bem-Vindo ao Portal ETEC News.");
+console.log("visitante ativo: " + nomevisitante);
+
+let desejanoticias = confirm("deseja receber notificações sobre as novas matérias?");
+console.log("Aceitou receber notificações: " + desejanoticias)
